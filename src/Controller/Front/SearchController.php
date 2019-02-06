6cbf3b3d774ffd83a3dba8f0216412ee640413cd
Qixin YING <ying.qixin@gmail.com>
2019-02-06 18:01:36 +0100

    feat(searchPage) : added routing

--- src/Controller/Front/SearchController.php
+++ src/Controller/Front/SearchController.php
@@ -14,13 +14,40 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/search")
+ */
 class SearchController extends AbstractController
 {
     /**
-     * @Route("/search", name="searchPage", methods="GET")
+     * @Route("/", name="searchPage", methods="GET")
      */
     public function index()
     {
         return $this->render('front/search.html.twig');
+    }
+
+    /**
+     * @Route("/", name="search", methods="POST")
+     */
+    public function search(Request $request) : Response
+    {
+        var_dump($request);
+        // $user = new User();
+        // $form = $this->createForm(UserType::class, $user);
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $entityManager = $this->getDoctrine()->getManager();
+        //     $entityManager->persist($user);
+        //     $entityManager->flush();
+
+        //     return $this->redirectToRoute('user_index');
+        // }
+
+        // return $this->render('user/new.html.twig', [
+        //     'user' => $user,
+        //     'form' => $form->createView(),
+        // ]);
     }
 }
