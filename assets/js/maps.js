76b4edebb7bc46a888d2f26bc19de9e4e87e366a
Qixin YING <ying.qixin@gmail.com>
2019-02-08 14:27:19 +0100

    fix(searchPage) : updated wsbpack with page-specific js/css

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,15 @@
 "use strict";
 
-var map;
+let map;
 function initmap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+     map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    map.locate({setView: true, maxZoom: 15});
 }
+
+
+
+
 initmap();
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ Encore
      * and one CSS file (e.g. _app.scss) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('map', './assets/js/maps.js')
+
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
