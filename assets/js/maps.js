3758c54f13d2a46d2ef481de8d4946f1b5842b7f
Qixin YING <ying.qixin@gmail.com>
2019-02-26 17:23:33 +0100

    fix(search) : added geocoding

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,13 +1,57 @@
 "use strict";
+import { EsriProvider  } from 'leaflet-geosearch';
 
-let map;
+var map;
+
 function initmap() {
-     map = L.map('map').setView([51.505, -0.09], 13);
+    map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     map.locate({setView: true, maxZoom: 15});
+
+    let query = window.location.search;
+    let adressQuery = query.split('&')[0];
+    let adress = adressQuery.split('=')[1].replace(/\+/g, ' ');
+    adress = decodeURIComponent(adress);
+    console.log(decodeURIComponent(adress));
+
+    // map.setView();
+    // const provider = new LocationIQProvider ({
+    //     params: {
+    //         key: 'e070475af59dda',
+    //     },
+    // });
+
+    const provider = new EsriProvider();
+    
+    provider
+        .search({ query: adress })
+        .then(findMeals);
 }
+
+function findMeals(result) {
+    var latlng = [result[0].y, result[0].x];
+    map.setView(latlng);
+
+    var circle = L.circle(latlng, {
+        color: '#96D5F0',
+        fillColor: '#96D5F0',
+        fillOpacity: 0.3,
+        radius: 1500
+    }).addTo(map);
+}
+
+
+// window.initGeocoding = function() {
+//     var geocoder = new google.maps.Geocoder;
+//     console.log(geocoder)
+//     geocoder.geocode({
+//         address: '5 rue robert lavergne asnières sur seine',
+//     })
+// }
+//
+//
 
 
 
