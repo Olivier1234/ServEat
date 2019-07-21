20fcaf0f95189f2503615b779a07ca1956803adb
Olivier1234 <olivier-2006@hotmail.fr>
2019-07-21 14:27:29 +0200

    optimisation blackfire

--- src/Controller/Front/PageController.php
+++ src/Controller/Front/PageController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller\Front;
 
+use App\Form\AddressType;
 use App\Repository\AddressRepository;
+use App\Repository\MessageRepository;
 use App\Repository\NotationRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Message;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Repository\MealRepository;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * @Route("/", name="front_page_")
